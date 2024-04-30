bae3ff8da7a6b67a591d9bdb80ef2e88cbe9bc3e
guns <self@sungpae.com>
2024-04-29 23:18:48 -0500

    Add explicit MIT License to top of files

--- src/listpicker/keyboard.py
+++ src/listpicker/keyboard.py
@@ -1,4 +1,24 @@
-# https://en.wikipedia.org/wiki/ANSI_escape_code#Terminal_input_sequences
+# The MIT License (MIT)
+#
+# Copyright © 2024 Sung Pae <self@sungpae.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 
 import termios
 import tty
@@ -8,6 +28,7 @@ import regex
 
 from .util import bytes_to_int
 
+# https://en.wikipedia.org/wiki/ANSI_escape_code#Terminal_input_sequences
 NAMED_KEY_BY_CODE = {
     0: "Ctrl-Space",
     **{n + 1: f"Ctrl-{chr(ord('A') + n)}" for n in range(26)},
